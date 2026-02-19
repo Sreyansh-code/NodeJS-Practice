43aa5dc3cb7749b49b5287450b1d8d3bc121fc94
Sreyansh-code <sreyanshtarway@gmail.com>
2026-02-19 13:57:02 +0530

    updated changes

--- index4a.js
+++ index4a.js
@@ -14,4 +14,20 @@ app.get("/x/:num1/y/:num2", (req, res) => {
     const sum = Number(req.params.num1) + Number(req.params.num2)
     res.send(sum)
 })
-app.listen(8080)
+
+app.get("/x/:num1/y/:num2/z/:num3", (req, res) => {
+    console.log(req.url);
+    console.log(req.params);
+    const sum = Number(req.params.num1) + Number(req.params.num2) + Number(req.params.num3)
+    res.send(sum)
+})
+
+app.get("/:w/:x/:y/:z", (req, res) => {
+    console.log(req.url);
+    console.log(req.params);
+    res.send("Hello Students")
+})
+
+app.listen(8080, () => {
+    console.log("server started");
+})
