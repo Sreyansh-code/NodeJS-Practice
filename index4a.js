2a95b37ad7ec38b6454e5621de332f6312c68775
Sreyansh-code <sreyanshtarway@gmail.com>
2026-02-19 13:47:55 +0530

    updated changes

--- index4a.js
+++ index4a.js
@@ -1,7 +1,17 @@
 import express from 'express'
 const app = express()
-app.get("/:id1/:id2", (req, res) => {
-    const sum = Number(req.params.id1) + Number(req.params.id2)
+
+app.get("/:num1/:num2", (req, res) => {
+    console.log(req.url);
+    console.log(req.params);
+    const sum = Number(req.params.num1) + Number(req.params.num2)
+    res.send(sum)
+})
+
+app.get("/x/:num1/y/:num2", (req, res) => {
+    console.log(req.url);
+    console.log(req.params);
+    const sum = Number(req.params.num1) + Number(req.params.num2)
     res.send(sum)
 })
 app.listen(8080)
