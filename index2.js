2258515b317cb0f343d7c029a43dd997bf25c037
Sreyansh-code <sreyanshtarway@gmail.com>
2026-02-19 13:39:44 +0530

    updated changes

---
+++ index2.js
@@ -0,0 +1,9 @@
+import express from 'express'
+const app = express()
+app.listen(8080, () => {
+    console.log("Server Started");
+})
+app.get("/", (req, res) => {
+    console.log(req.url);
+    res.send("respose from server from /")
+})
