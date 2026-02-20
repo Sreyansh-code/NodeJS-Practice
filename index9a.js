5d0ea6f590ce8d6082a54e0a23dde8c20c0429b1
Sreyansh-code <sreyanshtarway@gmail.com>
2026-02-20 16:08:15 +0530

    updated changes

---
+++ index9a.js
@@ -0,0 +1,73 @@
+// import express from 'express'
+// const app = express()
+
+// const auth = (req, res, next) => {
+//     if(req.url === "/1234"){
+//         next()
+//     }else{
+//         res.send('access denied')
+//     }
+// }
+
+// app.use(auth)
+
+// app.get("/1234", (req, res) => {
+//     res.send("welcome")
+// })
+
+// app.listen(8080, () => {
+//     console.log("server started");
+// })
+
+
+
+
+// import express from 'express'
+// const app = express()
+// app.use(express.json())
+
+// const auth = (req, res, next) => {
+//     if(req.body.token === 1234){
+//         next()
+//     }else{
+//         res.send('access denied')
+//     }
+// }
+
+// app.use(auth)
+
+// app.post("/", (req, res) => {
+//     res.send("welcome")
+// })
+
+// app.listen(8080, () => {
+//     console.log("server started");
+// })
+
+
+
+
+import express from 'express'
+const app = express()
+app.use(express.json())
+
+const auth = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    const token = authHeader.split(" ")[1]
+
+    if(token === "1234"){
+        next()
+    }else{
+        res.send('access denied')
+    }
+}
+
+app.use(auth)
+
+app.get("/", (req, res) => {
+    res.send("welcome")
+})
+
+app.listen(8080, () => {
+    console.log("server started");
+})
