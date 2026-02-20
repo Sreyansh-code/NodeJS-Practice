3618d582381b615a3c3a667145282d2ca7b6da3f
Sreyansh-code <sreyanshtarway@gmail.com>
2026-02-20 16:36:38 +0530

    updated changes

--- index9a.js
+++ index9a.js
@@ -47,24 +47,54 @@
 
 
 
+// import express from 'express'
+// const app = express()
+// app.use(express.json())
+
+// const auth = (req, res, next) => {
+//     const authHeader = req.headers.authorization
+//     const token = authHeader.split(" ")[1]
+
+//     if(token === "1234"){
+//         next()
+//     }else{
+//         res.send('access denied')
+//     }
+// }
+
+// app.use(auth)
+
+// app.get("/", (req, res) => {
+//     res.send("welcome")
+// })
+
+// app.listen(8080, () => {
+//     console.log("server started");
+// })
+
+
+
 import express from 'express'
 const app = express()
 app.use(express.json())
 
+const token = Math.round(Math.random()*10).toString()
+
 const auth = (req, res, next) => {
     const authHeader = req.headers.authorization
-    const token = authHeader.split(" ")[1]
-
-    if(token === "1234"){
+    const val = authHeader.split(" ")
+    if(val[1] === token){
         next()
     }else{
         res.send('access denied')
     }
 }
 
-app.use(auth)
+app.post("/login", (req, res) => {
+    res.send(token)
+})
 
-app.get("/", (req, res) => {
+app.get("/", auth, (req, res) => {
     res.send("welcome")
 })
 
