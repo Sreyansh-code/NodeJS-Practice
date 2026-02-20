5fae5c17066b2a8f79f36796d016fa971bfd95ff
Sreyansh-code <sreyanshtarway@gmail.com>
2026-02-20 09:11:32 +0530

    updated changes

--- index8.js
+++ index8.js
@@ -1,6 +1,7 @@
 import express from 'express'
 const app = express()
-const user = [{
+app.use(express.json())
+let users = [{
         id: 1,
         name: "John",
         email: "john@gmail.com",
@@ -19,5 +20,50 @@ const user = [{
         role: "admin"
 }]
 app.get("/", (req, res) => {
-    res.json(user)
-}).listen(8080)
+    res.json(users)
+})
+app.post("/", (req, res) => {
+    const user = req.body
+    users.push(user)
+    res.json(users)
+})
+app.delete("/:id", (req, res) => {
+    users = users.filter((user) => user.id !== Number(req.params.id))
+    res.json(users)
+})
+app.listen(8080, () => {
+    console.log("Server started");
+})
+
+
+
+
+
+// import express from 'express'
+// const app = express()
+// const users = [{
+//         id: 1,
+//         name: "John",
+//         email: "john@gmail.com",
+//         role: "student"
+//     },
+//     {
+//         id: 2,
+//         name: "Cathy",
+//         email: "cathy@gmail.com",
+//         role: "student"
+//     },
+//     {
+//         id: 3,
+//         name: "Mike",
+//         email: "mike@gmail.com",
+//         role: "admin"
+// }]
+// app.get("/", (req, res) => {
+//     res.json(users)
+// })
+// app.get("/:id", (req, res) => {
+//     const user = users.find((user) => user.id === Number(req.params.id))
+//     res.send(user)
+// })
+// app.listen(8080)
