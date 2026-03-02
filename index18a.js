591685d88aee4a47030f4e10290dcdc043c52dea
Sreyansh-code <sreyanshtarway@gmail.com>
2026-03-02 19:30:14 +0530

    updated changes

--- index18a.js
+++ index18a.js
@@ -4,6 +4,11 @@
 const num1 = Number(process.argv[2]);
 const num2 = Number(process.argv[3]);
 
+if (isNaN(num1) || isNaN(num2)) {
+  console.log("Please provide valid numbers.");
+  process.exit(1);
+}
+
 const sum = num1 + num2;
 
 console.log("Sum:", sum);
